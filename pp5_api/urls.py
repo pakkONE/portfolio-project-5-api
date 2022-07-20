2a6fda8ea70d5fe1d81b1d0915bca2d4c014d732
pakkONE <johan.pakarinen@gmail.com>
2022-07-20 07:54:06 +0000

    Added more dependencies and configured them

--- pp5_api/urls.py
+++ pp5_api/urls.py
@@ -20,5 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('dj-rest-auth/', include('dj_rest_auth.urls')),
-    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls'))
+    path('dj-rest-auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
