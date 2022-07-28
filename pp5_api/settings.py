d959d9bfff547a846b6fbfb6e5d2aca68a3fefa1
pakkONE <johan.pakarinen@gmail.com>
2022-07-28 09:20:13 +0000

    fix for CSRF_TRUSTED_ORIGINS

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -84,10 +84,8 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
-CSRF_TRUSTED_ORIGINS = [
-    os.environ.get('CLIENT_ORIGIN'),
-    os.environ.get('CLIENT_ORIGIN_DEV')
-]
+CSRF_TRUSTED_ORIGINS = os.environ.get('CLIENT_ORIGIN'), os.environ.get('CLIENT_ORIGIN_DEV')
+
 
 CORS_ALLOW_CREDENTIALS = True
 
