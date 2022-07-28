2957f9f93012e3f067d819f521d9b0173aaa6961
pakkONE <johan.pakarinen@gmail.com>
2022-07-28 10:46:49 +0000

    fixed bug with CLIENT_ORIGIN

--- pp5_api/settings.py
+++ pp5_api/settings.py
@@ -71,10 +71,6 @@ ALLOWED_HOSTS = [
 
 if 'CLIENT_ORIGIN' in os.environ:
     CORS_ALLOWED_ORIGINS = [
-        os.environ.get('CLIENT_ORIGIN')
-    ]
-elif 'CLIENT_ORIGIN' in os.environ:
-    CORS_ALLOWED_ORIGINS = [
         os.environ.get('CLIENT_ORIGIN'),
         os.environ.get('CLIENT_ORIGIN_DEV')
     ]
