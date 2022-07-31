bf04d975f0243eb2615a32bb3a3e5637739f964b
pakkONE <101711895+pakkONE@users.noreply.github.com>
2022-07-31 23:26:57 +0200

    Added comments_count to posts serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -13,6 +13,7 @@ class PostSerializer(serializers.ModelSerializer):
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     like_id = serializers.SerializerMethodField()
     likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def validate_image(self, image):
         """
@@ -61,5 +62,5 @@ class PostSerializer(serializers.ModelSerializer):
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
             'title', 'content', 'image', 'like_id',
-            'likes_count', 'tags'
+            'likes_count', 'tags', 'comments_count'
         ]
